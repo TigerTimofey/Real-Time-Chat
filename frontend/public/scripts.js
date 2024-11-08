424a615c60cbd5bc6bb3a3839b572facee4a4516
Timofey_Babisashvili <timofey.babisashvili@gmail.com>
2024-11-08 21:46:07 +0200

    local and prod links

--- frontend/public/scripts.js
+++ frontend/public/scripts.js
@@ -1,5 +1,11 @@
 // const socket = io("http://localhost:4000", {
-const socket = io("https://real-time-chat-coral-six.vercel.app", {});
+// const socket = io("https://real-time-chat-coral-six.vercel.app", {});
+const socket = io(
+  window.location.hostname === "localhost"
+    ? "http://localhost:4000"
+    : "https://real-time-chat-coral-six.vercel.app",
+  {}
+);
 
 const getRandomColor = () => {
   const letters = "0123456789ABCDEF";
